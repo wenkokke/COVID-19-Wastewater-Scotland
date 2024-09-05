569bd5d8ed336551d9ad0cd2fd213fbf663c3c57
Wen Kokke <wenkokke@users.noreply.github.com>
2024-09-05 14:23:46 +0100

    Get historical rainfall data

--- data/SEPA/Rainfall/get_data.py
+++ data/SEPA/Rainfall/get_data.py
@@ -34,22 +34,36 @@ else:
     RAINFALL_DATA_ENCODING = 'UTF-8'
 
 # Load the rainfall data:
-RAINFALL_DATA_COLUMNS = ['station_id', 'timestamp', 'value']
+RAINFALL_DATA_COLUMNS = ['station_no', 'timestamp', 'value']
 if not RAINFALL_DATA_FILE.exists():
-    with open(RAINFALL_DATA_FILE, 'w', newline='', encoding=RAINFALL_DATA_ENCODING) as rainfall_data_fp:
+    with open(
+            RAINFALL_DATA_FILE,
+            'w',
+            newline='',
+            encoding=RAINFALL_DATA_ENCODING
+        ) as rainfall_data_fp:
         RAINFALL_DATA_WRITER = csv.DictWriter(rainfall_data_fp, fieldnames=RAINFALL_DATA_COLUMNS)
         RAINFALL_DATA_WRITER.writeheader()
         for station_data in STATIONS_DATA:
             station_no = station_data['station_no']
             station_name = station_data['station_name']
-            stdout.write("[   ] {:<40}".format(station_name))
+            stdout.write("[   ] {:<40}".format(
+                station_name
+            ))
             station_data_request_url = f"{API_BASEURL}/Daily/{station_no}?all=true"
             station_data_response = requests.get(station_data_request_url)
             stdout.write("\r")
-            stdout.write("[{:<3}] {:<40}".format(station_data_response.status_code, station_name))
+            stdout.write("[{:<3}] {:<40}".format(
+                station_data_response.status_code,
+                station_name
+            ))
             station_data_samples = station_data_response.json()
             stdout.write("\r")
-            stdout.write("[{:<3}] {:<40} ({} samples)".format(station_data_response.status_code, station_name, len(station_data_samples)))
+            stdout.write("[{:<3}] {:<40} ({} samples)".format(
+                station_data_response.status_code,
+                station_name,
+                len(station_data_samples)
+            ))
             stdout.write("\n")
             if station_data_samples:
                 for station_data_sample in station_data_samples:
